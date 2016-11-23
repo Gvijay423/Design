8ae6ed94e5e74f0d11ea5b0d75356efe6b4c231c
RajuKottedi <raju.kottedi@gmail.com>
2016-11-23 16:55:45 +0300

    Implemented right bar and hiding -showing.
    Implemented right bar and hiding -showing.

--- js/app.js
+++ js/app.js
@@ -4,5 +4,30 @@
 $(document).ready(function(){
     $('#menubtn').click(function(){
         $('#innerContent').fadeToggle();
-    })
+        $('#sidetoggle').fadeToggle();
+    });
+
+    $('#sidebtn').click(function(){
+        $('body').toggleClass('fullscreen');
+    });
+
+    $('#contractbtn').click(function(){
+        if($('body').hasClass('rightpanel')){
+            $('body').toggleClass('rightpanel');
+        }
+        else{
+            if($('body').hasClass('fullscreen')) {
+                $('body').toggleClass('fullscreen');
+            }
+        }
+    });
+
+    $('#rightpnlbtn').click(function(){
+        $('body').toggleClass('rightpanel');
+    });
+
+    $('#rightclosebtn').click(function(){
+        $('body').toggleClass('rightpanel');
+    });
+
 });
