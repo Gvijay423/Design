5d49066cf5a52345269166e6845788d782bf724d
RajuKottedi <raju.kottedi@gmail.com>
2016-11-27 01:09:36 +0530

    End of Design. Can be used as a reference for css

--- js/app.js
+++ js/app.js
@@ -2,17 +2,20 @@
  * Created by BKottedi on 11/22/2016.
  */
 $(document).ready(function(){
+	
     $('#menuico').click(function(){
         $('#innerContent').fadeToggle();
         $('#sidetoggle').fadeToggle();
         $('#menubar').toggleClass('menu');
         $('#menubar').toggleClass('home');
+        $('#homebtn').toggle();
     });
     $('#homeico').click(function(){
         $('#innerContent').fadeToggle();
         $('#sidetoggle').fadeToggle();
         $('#menubar').toggleClass('home');
         $('#menubar').toggleClass('menu');
+        $('#homebtn').toggle();
     });
 
     $('#sidebtn').click(function(){
